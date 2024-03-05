8554ea77f36d3188f412e1c6f486e190b16281af
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-03-05 08:30:53 +0700

    feat(category): implement CRUD operations for category data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,11 +82,11 @@
                         @endcan
 
                         @can('categories.index')
-                        <li class="{{ setActive('admin/category') }}"><a class="nav-link" href="#"><i
-                                    class="fas fa-folder"></i>
-                                <span>Kategori</span></a></li>
+                        <li class="{{ setActive('admin/category') }}"><a class="nav-link"
+                                href="{{ route('admin.category.index') }}"><i class="fas fa-folder"></i>
+                                <span>Kategori</span></a>
+                        </li>
                         @endcan
-
                         @can('events.index')
                         <li class="{{ setActive('admin/event') }}"><a class="nav-link" href="#"><i
                                     class="fas fa-bell"></i>
