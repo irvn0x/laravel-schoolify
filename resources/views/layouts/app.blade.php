ad4ddb8533f8712fb41b3d563bf64898ba0164d7
Irvansyah <ilhamirvansyah.dev@gmail.com>
2024-03-05 21:32:56 +0700

    feat(post): implement CRUD operations for post data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,9 +69,10 @@
                         <li class="{{ setActive('admin/dashboard') }}"><a class="nav-link"
                                 href="{{ route('admin.dashboard.index') }}"><i class="fas fa-tachometer-alt"></i>
                                 <span>Dashboard</span></a></li>
+
                         @can('posts.index')
-                        <li class="{{ setActive('admin/post') }}"><a class="nav-link" href="#"><i
-                                    class="fas fa-book-open"></i>
+                        <li class="{{ setActive('admin/post') }}"><a class="nav-link" href="{{ route('admin.post.index') }}
+                            "><i class="fas fa-book-open"></i>
                                 <span>Berita</span></a></li>
                         @endcan
 
